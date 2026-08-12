782e2f1ce5833324b981ffd8ece277740c2c82ee
abimaelhernandez <hernandezabimael364@gmail.com>
2026-08-11 19:28:19 -0600

    feat: create ghome page

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import Link from 'next/link'
+// import Link from 'next/link'
+import Hero from '@/components/Hero'
+import InfoBoxes from '../components/InfoBoxes'
+import Footer from '@/components/Footer'
 
 const HomePage = () => {
   console.log('gome page')
   return (
-    <div className='bg-blue-500'> 
-      <h1 className='text-white text-3xl'>
-        Weork on the home page
-      </h1>
-      <Link href='/properties'>  Show  properties </Link>
-    </div>
+    <>
+      <Hero/>
+      <InfoBoxes/>
+      <Footer/>
+    </>
   )
 }
 
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -217,27 +217,24 @@ const NavBar = () => {
                 <Link
                   href="/"
                   className={`${pathName === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
-                  >
-                    Home
-                  </Link>
+                >
+                  Home
+                </Link>
                 
                 <Link
                   href="/properties"
                   className={`${pathName === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
-                  >
-                    Properties
-                 </Link>
-                {isLoggedIn && 
-                  (
-                    <Link
-                      href="/properties/add"
-                      className={`${pathName === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
-                      >
-                        Add Property
-                    </Link>
-                  )
-                }
-                
+                >
+                  Properties
+                </Link>
+                {isLoggedIn && (
+                  <Link
+                    href="/properties/add"
+                    className={`${pathName === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
+                    >
+                      Add Property
+                  </Link>
+                )}
                 {!isLoggedIn && (
                   <button
                     className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
