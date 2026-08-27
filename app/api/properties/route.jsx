2f4ac5703bcd3991ed324da782a1a666f81504d7
abimaelhernandez <hernandezabimael364@gmail.com>
2026-08-27 16:52:23 -0600

    obtain info from connected DB

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,10 +1,16 @@
-import connectDB from '../../config/database'
-// import ConnectDB from '../config/database'
+import connectDB from '@/config/database'
+import Property from '@/models/Property'
 
+//  GET /api/properties
 export const GET  = async (request) => {
   try {
     await connectDB ()
-    return new Response(JSON.stringify({message: 'hellow word', status: 200 }))
+
+    const properties = await Property.find({})
+    
+    console.log(properties)
+    
+    return new Response(JSON.stringify(properties, {status: 200 }))
   } catch (error) {
     console.log(error)
     return new Response('Something is wrong', { status: 500})
