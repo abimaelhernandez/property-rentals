50abe2dd55bfb7ffda6947b7d5869792eba18751
abimaelhernandez <hernandezabimael364@gmail.com>
2026-08-27 13:21:39 -0600

    create hom page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '@/components/Hero'
 import InfoBoxes from '../components/InfoBoxes'
+import HomeProperties from '../components/HomeProperties'
 
 const HomePage = () => {
   console.log('gome page')
@@ -8,6 +9,7 @@ const HomePage = () => {
     <>
       <Hero/>
       <InfoBoxes/>
+      <HomeProperties/>s
       {/* <Footer/> */}
     </>
   )
